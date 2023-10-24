37c5f0785cdd0a04caefd31a4282bad9994b4198
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2023-10-25 02:04:14 +0300

    O(N^2 + N)

--- big_o.py
+++ big_o.py
@@ -10,7 +10,10 @@ def print_items(n):
     ## O(N^2) 
     for i in range(n):
         for j in range(n):
-            print(i,j);
+            print(i,j)
+    ##    O(N^2 + N)     
+    for k in range(n):
+        print(k)
         
 # DO NOT CHANGE THIS LINE:
 print_items(10)
