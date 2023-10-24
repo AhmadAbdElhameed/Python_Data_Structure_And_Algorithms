3164345b2db0d980ee663ecd0d25f45a718c9b2c
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2023-10-25 01:58:34 +0300

    O(N^2)

--- big_o.py
+++ big_o.py
@@ -1,11 +1,16 @@
 def print_items(n):
     ## O(N)
-    for i in range(n):
-        print(i);
+    # for i in range(n):
+    #     print(i);
         
     ## O(2N)
-    for j in range(n):
-        print(j);
+    # for j in range(n):
+    #     print(j);
+        
+    ## O(N^2) 
+    for i in range(n):
+        for j in range(n):
+            print(i,j);
         
 # DO NOT CHANGE THIS LINE:
 print_items(10)
