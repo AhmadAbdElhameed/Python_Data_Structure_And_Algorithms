6be83ff42192f2ab6901ee4b221ef24a2f3bea3d
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2023-10-25 01:55:53 +0300

    if O(2N)

--- big_o.py
+++ big_o.py
@@ -1,7 +1,11 @@
 def print_items(n):
+    ## O(N)
     for i in range(n):
         print(i);
         
+    ## O(2N)
+    for j in range(n):
+        print(j);
         
 # DO NOT CHANGE THIS LINE:
 print_items(10)
